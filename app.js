25b2bd9c2779e10544e646847af7ab86cb08116e
Antonio Montalvo <montalvocode@yahoo.com>
2016-08-11 19:22:02 -0700

    up

---
+++ app.js
@@ -0,0 +1,8 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDkGmRBIcRfCLIT-RJPmGi6qvM8TKRKfkM",
+    authDomain: "employee-group.firebaseapp.com",
+    databaseURL: "https://employee-group.firebaseio.com",
+    storageBucket: "employee-group.appspot.com",
+  };
+  firebase.initializeApp(config);
